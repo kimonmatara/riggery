fb1e0115dc72affb0b62445f241c010e4ee026f6
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2025-09-14 22:31:26 +0300

    Added getInfluence()

--- src/riggery/core/plugtypes/nurbsSurface.py
+++ src/riggery/core/plugtypes/nurbsSurface.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 import maya.api.OpenMaya as om
 
 from ..plugtypes import __pool__ as plugs
@@ -5,6 +6,22 @@ from ..plugtypes import __pool__ as plugs
 
 class NurbsSurface(plugs['Geometry']):
 
+    #-------------------------------------|    Constructor(s)
+
+    @classmethod
+    def fromSegmentedLoft(cls, curveInputs:Iterable):
+        """
+        Does the MPC-style thing of generating a lofted surface by performing
+        pairwise lofts and then attaching the surfaces. This prevents EP-style
+        overcompensation along the edge.
+
+        :return: The surface output.
+        """
+        ...
+
+    #-------------------------------------|    Esoteric queries
+
     def _getData(self) -> om.MObject:
         return self._getSamplingPlug(
             ).asMDataHandle().asNurbsSurfaceTransformed()
+
