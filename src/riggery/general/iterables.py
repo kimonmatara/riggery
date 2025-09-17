4107652dfd34ba8113c6dfffe831724103a8cf99
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2025-09-17 14:43:36 +0300

    Added partial_reorder()

--- src/riggery/general/iterables.py
+++ src/riggery/general/iterables.py
@@ -43,3 +43,29 @@ def crop_overlaps(groups):
     if len(groups) < 2:
         return list(groups)
     return [group[:-1] for group in groups[:-1]] + [groups[-1]]
+
+def partial_reorder(items:Iterable, items_to_reorder:Iterable) -> list:
+    """
+    :param items: the full item list
+    :param items_to_reorder: a sub-selection of elements from *items*, in the
+        desired order
+    :raises ValueError: Duplicate items in *items* or *items_to_reorder*.
+    :raises ValueError: Some members of *items_to_reorder* are not members of
+        *items*.
+    :return: The members of *items*, reordered so that the subset of members in
+        *items_to_reorder* is at the specified order, with the relative position
+        of every other member preserved.
+    """
+    _items = set(items)
+    _items_to_reorder = set(items_to_reorder)
+
+    if len(_items) != len(items) or len(_items_to_reorder) != items_to_reorder:
+        raise ValueError("Duplicate items")
+
+    if not _items_to_reorder.issubset(_items):
+        raise ValueError("Non-member items")
+
+    head = items[:first_reord_index]
+    middle = items_to_reorder
+    tail = [x for x in items if x not in head + middle]
+    return head + middle + tail
