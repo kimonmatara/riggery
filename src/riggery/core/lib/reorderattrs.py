49bce2c18195a9ee0a737f425855ea36b13442ba
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2025-09-17 22:46:05 +0300

    Reverted reordering algo revision

--- src/riggery/core/lib/reorderattrs.py
+++ src/riggery/core/lib/reorderattrs.py
@@ -88,7 +88,7 @@ class ReorderableAttr:
     @property
     def macro(self) -> dict:
         return self._macro
-    
+
     @property
     def node(self) -> om.MObject:
         return self._node
@@ -368,12 +368,19 @@ def reorderAttrs(node:om.MObject, attrNames:list[str]) -> list[om.MPlug]:
     :return: :class:`~maya.api.OpenMaya.MPlug` instances for the specified
         attributes.
     """
+    print('The requested attr names are:')
+    for x in attrNames:
+        print(x)
     allAttrs = ReorderableAttr.fromNode(node)
+    print('All attrs are:')
+    for x in allAttrs:
+        print(x)
 
     # The valmap is required because of a bug whereby some attribute values
     # change after the .remove() step, not sure why
 
     valmap = {}
+
     for attr in allAttrs:
         try:
             valmap[attr.attrPath] = m.getAttr(attr.attrPath)
