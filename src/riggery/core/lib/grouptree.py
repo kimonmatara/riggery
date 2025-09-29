7aaf8a550dae364b3934b784e667dac712b09315
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2025-09-29 23:08:54 +0300

    Tidied up GroupTree implementation

--- src/riggery/core/lib/grouptree.py
+++ src/riggery/core/lib/grouptree.py
@@ -1,101 +1,125 @@
 """Contains utility classes for quick construction of DAG hierarchies."""
 
-from typing import Optional
+from typing import Optional, Generator
+
 from ..elem import Elem
 from ..nodetypes import __pool__ as nodes
 from ..lib import names as _nm
 
+
 class GroupTree:
 
-    def __init__(self, root=None, /):
-        if root:
-            root = Elem(root)
-        self._node = root
+    #-----------------------------------------|    Inst
 
-    def node(self):
-        return self._node
+    def __init__(self, *, key=None, node=None, parent=None):
+        self._key = key
+
+        if node is not None:
+            node = nodes['Transform'](node)
+
+        self._node = node
+        self._parent = parent
+
+    #-----------------------------------------|    Properties
+
+    @property
+    def key(self) -> Optional[str]:
+        return self._key
+
+    @property
+    def parent(self) -> Optional['GroupTree']:
+        return self._parent
+
+    #-----------------------------------------|    Get branches
+
+    def __getitem__(self, key):
+        return GroupTree(key=key, parent=self)
+
+    #-----------------------------------------|    Traversal
 
     @property
     def root(self):
-        return self
+        if self.isRoot():
+            return self
+
+        for parent in self.parents:
+            if parent.isRoot():
+                return parent
 
     def isRoot(self) -> bool:
-        return True
+        return not self._key
 
-    def __getitem__(self, key:str):
-        return GroupLeaf(self, key)
+    @property
+    def keys(self) -> list[str]:
+        if self.isRoot():
+            return []
+
+        out = [self.key]
+        for parent in self.parents:
+            if parent.isRoot():
+                break
+            out.append(parent.key)
+        return list(reversed(out))
+
+    @property
+    def parents(self) -> Generator['GroupTree', None, None]:
+        current = self
+        while True:
+            current = current.parent
+            if current is None:
+                break
+            yield current
+
+    @property
+    def stack(self) -> list:
+        return list(reversed(list(self.parents))) + [self]
+
+    def node(self, **xformAttrs):
+        """
+        Retrieves, or creates, a node at this tree depth.
+
+        :param \*\*xformAttrs: a dictionary of attribute names, and values, to
+            quickly configure the node.
+        """
+        if self.isRoot():
+            return self._node
+        out = nodes['Transform'].createFromDagPath(str(self))
+        if xformAttrs:
+            out.setAttrs(**xformAttrs)
+        return out
+
+    #-----------------------------------------|    Repr
+
+    def __str__(self):
+        """
+        The projected DAG path for the node at this tree depth.
+        """
+        stack = self.stack
+        root = stack.pop(0)
+
+        namespace = str(root._node.namespace)
+
+        if not namespace.endswith(':'):
+            namespace += ':'
+        prefix = root._node.prefix
+
+        suffix = '_' + _nm.TYPESUFFIXES['transform']
+
+        keys = [branch.key for branch in stack]
+        elems = [root._node.longName()] + [f"{namespace}{prefix}{key}{suffix}"
+                                           for key in keys]
+        return '|'.join(elems)
 
     def __repr__(self):
-        return "{}({})".format(type(self).__name__,
-                               repr(str(self._node)) if self._node else '')
+        _node = self._node
+        if _node is None:
+            _node = 'None'
+        else:
+            _node = repr(str(_node))
 
+        if self.isRoot():
+            return "{}(node={})".format(self.__class__.__name__, _node)
 
-class GroupLeaf:
-
-    def __init__(self, parent:GroupTree, key:str):
-        self._parent = parent
-        self._key = key
-
-    @property
-    def parent(self):
-        return self._parent
-
-    @property
-    def key(self):
-        return self._key
-
-    def isRoot(self):
-        return False
-
-    def node(self, **xformAttrs) -> str:
-        """
-        Retrieves, or creates, the group at the current level.
-
-        :param \*\*xformAttrs: optional attibute edits, as keyword arguments;
-            for example, to set 'inheritsTransform' to False on retrieval, do:
-
-            .. code-block:: python
-
-                tree['util'].node(it=False)
-        """
-        keys = []
-        current = self
-        root = None
-
-        while True:
-            keys.append(current.key)
-            current = current.parent
-            if current.isRoot():
-                root = current
-                break
-
-        rootNode = root.node()
-        namespace = None
-        prefix = None
-
-        if rootNode:
-            namespace = rootNode.namespace
-            prefix = rootNode.shortName(sns=True, sts=True)
-
-        out = []
-        if rootNode:
-            out.append(rootNode.absoluteName())
-
-        ts = _nm.TYPESUFFIXES['transform']
-
-        for key in reversed(keys):
-            name = f"{key}_{ts}"
-            if prefix:
-                name = f"{prefix}_{name}"
-            if namespace:
-                name = f"{namespace}:{name}"
-            out.append(name)
-
-        dagPath = '|'.join(out)
-        node = nodes['Transform'].createFromDagPath(dagPath)
-        if xformAttrs:
-            node.setAttrs(**xformAttrs)
-        return node
-
-    def __getitem__(self, key):
-        return GroupLeaf(self, key)
+        root = self.root
+        tail = ''.join([f"['{key}']" for key in self.keys])
+        return f"{repr(root)}{tail}"
