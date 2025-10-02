bcbc44f6657ddfdcc09b3c98931200f157db2d52
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2025-10-02 14:56:23 +0300

    Made callbacks class-level; added 'predicate' argument; added ofType() method

--- src/riggery/core/lib/nodetracker.py
+++ src/riggery/core/lib/nodetracker.py
@@ -1,81 +1,123 @@
 """Tools for automatic node capture / collection."""
 
-from typing import Generator
+from typing import Generator, Union
+
 import maya.api.OpenMaya as om
+import maya.cmds as m
+
 from riggery.general.functions import short
+from riggery.general.iterables import expand_tuples_lists
+import riggery.internal.nodeinfo as _ni
 from ..nodetypes import __pool__ as _nodes
 
+
 class NodeTracker:
-    """
-    Context manager. Collects nodes created during the block. To get
-    :class:`~riggery.core.elem.Elem` instances for the nodes, either iterate over
-    the object or cast it to a list.
 
-    .. code-block:: python
+    __stack__ = []
+    __callbacks__ = []
 
-        with NodeTracker() as nodes:
-            m.createNode('joint')
-            m.createNode('joint')
+    #-------------------------------------|    Inst
 
-        print(list(nodes))
-        # Result: [Joint('joint1'), Joint('joint2')]
-    """
-
-    @short(nodeType='nt')
-    def __init__(self, nodeType:str='dependNode'):
+    def __init__(self, shallow:bool=False, predicate=None):
         """
-        :param nodeType / nt: the type of node to track; defaults to
-            'dependNode' (i.e. every dependency node subtype)
+        :param shallow: don't include nodes contributed by child node trackers;
+            defaults to False
+        :param predicate: an optional callable, that will receive an MObject
+            for a node, and should return True if the node should be tracked or
+            False if it shouldn't; defaults to None
         """
-        self._nodeType = nodeType
-        self._nodes:list = []
-        self._callbacks = []
+        self._nodes = []
+        self._shallow = shallow
+
+        if predicate is None:
+            predicate = lambda mObject: True
+        self._predicate = predicate
+
+    #-------------------------------------|    Context
 
     def __enter__(self):
         self._nodes.clear()
-        self._startCallbacks()
+        NodeTracker.__stack__.append(self)
+
+        if len(NodeTracker.__stack__) == 1:
+            self._startCallbacks()
+
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self._stopCallbacks()
+        if len(NodeTracker.__stack__) == 1:
+            self._stopCallbacks()
+
+        NodeTracker.__stack__.remove(self)
+
         return False
 
-    #-------------------------------------|    Start / stop
+    #-------------------------------------|    Add / remove methods
 
-    def _startCallbacks(self):
-        self._callbacks = [
-            om.MDGMessage.addNodeAddedCallback(
-                self._nodeAdded,
-                self._nodeType
-            ),
-            om.MDGMessage.addNodeRemovedCallback(
-                self._nodeRemoved,
-                self._nodeType
-            ),
+    def _addNode(self, node:om.MObject):
+        if self._predicate(node):
+            self._nodes.append(node)
+
+    def _removeNode(self, node:om.MObject):
+        if self._predicate(node):
+            self._nodes.remove(node)
+
+    #-------------------------------------|    Callbacks
+
+    @classmethod
+    def _nodeAdded(cls, node:om.MObject, *args):
+        for i, tracker in enumerate(NodeTracker.__stack__[::-1]):
+            if tracker._shallow and i > 0:
+                break
+            tracker._addNode(node)
+
+    @classmethod
+    def _nodeRemoved(cls, node:om.MObject, *args):
+        for i, tracker in enumerate(NodeTracker.__stack__[::-1]):
+            if tracker._shallow and i > 0:
+                break
+            tracker._removeNode(node)
+
+    @classmethod
+    def _startCallbacks(cls):
+        NodeTracker.__callbacks__[:] = [
+            om.MDGMessage.addNodeAddedCallback(cls._nodeAdded, 'dependNode'),
+            om.MDGMessage.addNodeRemovedCallback(cls._nodeRemoved, 'dependNode')
         ]
 
-    def _stopCallbacks(self):
-        om.MMessage.removeCallbacks(self._callbacks)
-        self._callbacks.clear()
+    @classmethod
+    def _stopCallbacks(cls):
+        om.MMessage.removeCallbacks(NodeTracker.__callbacks__)
+        NodeTracker.__callbacks__.clear()
 
-    #-------------------------------------|    Populating
+    #-------------------------------------|    List interface
 
-    def _nodeAdded(self, node:om.MObject, *args):
-        self._nodes.append(node)
+    def ofType(self, type:Union[str, list[str]]) -> Generator:
+        """
+        In contrast to :meth:`__iter__`, only yields nodes that match the
+        specified node type(s).
 
-    def _nodeRemoved(self, node:om.MObject, *args):
-        self._nodes.remove(node)
+        :param type: one or more node types (e.g. 'camera')
+        """
+        if type is None:
+            cls = _nodes['DependNode']
 
-    #-------------------------------------|    List-like interface
+            for node in self._nodes:
+                yield cls.fromMObject(node)
+        else:
+            acceptedTypes = set(expand_tuples_lists(type))
+
+            for node in self._nodes:
+                nodeType = om.MFnDependencyNode(node).typeName
+                nodeTypes = set(m.nodeType(nodeType,
+                                           inherited=True,
+                                           isTypeName=True))
+                if nodeTypes.intersection(acceptedTypes):
+                    cls = _nodes[nodeType[0].upper()+nodeType[1:]]
+                    yield cls.fromMObject(node)
 
     def __iter__(self):
-        T = _nodes['DependNode']
+        cls = _nodes['DependNode']
 
         for node in self._nodes:
-            yield T.fromMObject(node)
-
-    def __len__(self):
-        return len(list(self._nodes))
-
-    def __bool__(self):
-        return len(self) > 0
+            yield cls.fromMObject(node)
