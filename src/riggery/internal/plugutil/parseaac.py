90a3a7058e9afac8d1e73c55e6e225f670ab285c
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2025-10-15 01:22:07 +0300

    Reimplemented parseAddAttrCmd

--- src/riggery/internal/plugutil/parseaac.py
+++ src/riggery/internal/plugutil/parseaac.py
@@ -2,35 +2,31 @@
 Defines a function to parse the return of
 :meth:`~maya.api.OpenMaya.MFnAttribute.getAddAttrCmd`.
 """
+from typing import Union
+import re
+
+def interpretValue(value:str) -> Union[str, bool, float, int]:
+    mt = re.match(r"^(true|false)$", value)
+    if mt:
+        return {'true': True, 'false': False}[mt.group(1)]
+    else:
+        mt = re.match(r"^\"(.*?)\"$", value)
+        if mt:
+            return mt.group(1)
+        else:
+            try:
+                return int(value)
+            except ValueError:
+                try:
+                    return float(value)
+                except:
+                    return value
 
 def parseAddAttrCmd(cmd:str) -> dict:
     """
     Parses the string returned by
     :meth:`~maya.api.OpenMaya.MFnAttribute.getAddAttrCmd` into a dictionary.
     """
-    cmd = cmd.strip().strip(';')
-    pairs = cmd.split(' -')
-
-    out = {}
-    del(pairs[0])
-
-    for pair in pairs:
-        pair = pair.split(' ')
-        key = pair.pop(0)
-        value = ' '.join(pair)
-        value = value.strip('"')
-        if value == '':
-            value = True
-        else:
-            try:
-                value = int(value)
-            except ValueError:
-                try:
-                    value = float(value)
-                except ValueError:
-                    value = {'true': True,
-                             'false': False}.get(value, value)
-
-        out[key] = value
-
-    return out
+    pat = r"-([a-zA-Z]+[a-zA-Z0-9]?) (true|false|\".*?\"|0-9|\-?[0-9]?.*?(?: |^))"
+    return dict([(a, interpretValue(b.strip()))
+                 for a, b in re.findall(pat, cmd)])
