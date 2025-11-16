13201401342c93e68b5c322c110ea8114850048f
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2025-11-16 21:12:30 +0200

    parity properties

--- src/riggery/core/nodetypes/mesh.py
+++ src/riggery/core/nodetypes/mesh.py
@@ -5,14 +5,20 @@ import maya.cmds as m
 
 
 class Mesh(SurfaceShape):
+
+    #-------------------------------------|    UV set get / set
     
     def getUVSet(self) -> str:
         """Returns the name of the current UV set."""
         return m.polyUVSet(str(self), q=True, currentUVSet=True)
 
+    getCurrentUVSetName = getUVSet # for parity with PyMEL
+
     def setUVSet(self, uvSet:str):
         """Sets the current UV set."""
         m.polyUVSet(str(self), e=True, currentUVSet=uvSet)
         return self
+
+    setCurrentUVSetName = setUVSet # for parity with PyMEL
 
     uvSet = property(getUVSet, setUVSet)
