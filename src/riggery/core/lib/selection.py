c93c84699a29d2b33dfc1bd150228f4c29952e9c
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2026-01-08 00:49:42 +0200

    Added selection.py

---
+++ src/riggery/core/lib/selection.py
@@ -0,0 +1,42 @@
+from functools import wraps
+from riggery.general.iterables import without_duplicates
+import maya.cmds as m
+
+def _fuzzySelect(items):
+    if items:
+        allMatches = []
+
+        for item in items:
+            matches = m.ls(item)
+
+            if matches:
+                allMatches += matches
+            else:
+                if '|' in item:
+                    item = item.split('|')[-1]
+                    matches = m.ls(item)
+
+                    if matches:
+                        allMatches += matches
+
+        m.select(list(without_duplicates(allMatches)), replace=True)
+    else:
+        m.select(cl=True)
+
+
+class Selection:
+    def __enter__(self):
+        self._prevSelection = m.ls(sl=True)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        _fuzzySelect(self._prevSelection)
+        return False
+
+def keepsel(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        with Selection():
+            result = f(*args, **kwargs)
+        return result
+    return wrapper
