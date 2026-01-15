2cd547911bb36be8225c03b92b3b05fce2ff2f4f
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2026-01-15 22:07:01 +0200

    Added closestPoint()

--- src/riggery/core/nodetypes/nurbsSurface.py
+++ src/riggery/core/nodetypes/nurbsSurface.py
@@ -1,13 +1,35 @@
+from ..datatypes import __pool__ as _data
 from ..nodetypes import __pool__ as nodes
 SurfaceShape = nodes['SurfaceShape']
 
 import maya.api.OpenMaya as om
 import maya.cmds as m
 
+from riggery.general.functions import short
+
 
 class NurbsSurface(SurfaceShape):
-    
+
     #-------------------------------------|    Geometry queries
+
+    @short(worldSpace='ws')
+    def closestPoint(self,
+                     point:'_data.Point',
+                     worldSpace:bool=False
+                     ) -> tuple['_data.Point', float, float]:
+        """
+        :param point: the reference point
+        :param worldSpace/ws: calculate in world-space; defaults to False
+        :return: Tuple of point, u param, v param
+        """
+        if worldSpace:
+            space = om.MSpace.kWorld
+        else:
+            space = om.MSpace.kObject
+
+        fn = self.__apimfn__()
+        result = fn.closestPoint(om.MPoint(point), space=space)
+        return _data['Point'].fromApi(result[0]), result[1], result[2]
 
     def numCVs(self) -> int:
         """:return: The number of CVs on this surface."""
