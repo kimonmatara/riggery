6cb24726aa60806016f8937190c6d11614659c71
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2026-01-28 02:01:13 +0200

    cleanup

--- src/riggery/core/nodetypes/skinCluster.py
+++ src/riggery/core/nodetypes/skinCluster.py
@@ -747,8 +747,6 @@ class SkinCluster(GeometryFilter):
         kwargs = {}
 
         if includeBlendWeights:
-            print("include blend weights is on!")
-
             if attribute is None:
                 attribute = []
             else:
@@ -756,8 +754,6 @@ class SkinCluster(GeometryFilter):
 
             attribute.append('blendWeights')
             indicesToRemove = self._padBlendWeights()
-
-            print("attribute resolved as:")
 
             kwargs['at'] = attribute
 
