2bc98719936f66db9cd9d59c5717524a32d21612
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2026-02-23 17:09:25 +0200

    Added basic constructor and add()

--- src/riggery/core/nodetypes/objectSet.py
+++ src/riggery/core/nodetypes/objectSet.py
@@ -1,13 +1,56 @@
-from typing import Iterator
+from typing import Iterator, Optional
+
 from ..nodetypes import __pool__ as nodes
-DependNode = nodes['DependNode']
+from ..lib import names as _nm
+
+from riggery.general.iterables import expand_tuples_lists, without_duplicates
+from riggery.general.functions import short
 
 import maya.cmds as m
+
+DependNode = nodes['DependNode']
 
 
 class ObjectSet(DependNode):
 
-    #---------------------------------|    Member queries
+    #---------------------------------|    Constructor
+
+    @classmethod
+    @short(name='n', parent='p')
+    def create(cls, *members, name:Optional[str]=None, parent=None):
+        kwargs = {'empty': True}
+
+        if name:
+            kwargs['name'] = name
+        else:
+            if _nm.Name.__elems__:
+                kwargs['name'] = _nm.Name.evaluate(
+                    typeSuffix=cls.__typesuffix__
+                )
+
+        node = nodes['ObjectSet'](m.sets(**kwargs))
+
+        if members:
+            node.add(members)
+
+        if parent:
+            nodes['ObjectSet'](parent).add(node)
+
+        return node
+
+    #---------------------------------|    Add members
+
+    def add(self, *members) -> 'ObjectSet':
+        """Adds the specified members to this set."""
+        members = list(
+            without_duplicates(map(str, expand_tuples_lists(*members)))
+        )
+        if members:
+            m.sets(members, e=True, fe=str(self))
+
+        return self
+
+    #---------------------------------|    Get members
 
     def iterDagSetMembers(self) -> Iterator:
         """
