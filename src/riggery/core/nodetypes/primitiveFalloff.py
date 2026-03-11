d7552b6a7eb8f2436fbd87302b0f7d5b702058a4
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2026-03-11 19:10:01 +0200

    cleanup

--- src/riggery/core/nodetypes/primitiveFalloff.py
+++ src/riggery/core/nodetypes/primitiveFalloff.py
@@ -1,19 +1,24 @@
-from typing import Union, Optional
+from typing import Union, Optional, Literal
+
+from riggery.general.functions import short
+
 from ..plugtypes import __pool__ as plugs
 from ..nodetypes import __pool__ as nodes
+from .falloffBase import FalloffBase
+
 Transform = nodes['Transform']
 
 import maya.cmds as m
 
 
-class PrimitiveFalloff(Transform):
+class PrimitiveFalloff(FalloffBase, Transform):
     
     #---------------------------------|    Constructor
 
     @classmethod
     @short(name='n')
     def create(cls,
-               primitive:Literal[0, 1, 'sphere', 'plane', 'Sphere', 'Plane']=0,
+               primitive:Literal[0, 1, 'Sphere', 'Plane']=0,
                *,
                name:Optional[str]=None,
                **attrConfig):
@@ -21,11 +26,9 @@ class PrimitiveFalloff(Transform):
         Basic constructor.
 
         :param name/n: an optional explicit name for the node
+        :param primitive: the primitive type; one of 0, 1, 'Sphere' or 'Plane';
+            defaults to 0
         :param \*\*attrConfig: optional values or inputs for node attributes
         :return: The configured ``PrimitiveFalloff`` node.
         """
-        node = cls.createNode(name=name)
-        for k, v in attrConfig.items():
-            node.attr(k).put(v)
-
-        return node
+        return cls.createNode(name=name, primitive=primitive, **attrConfig)
