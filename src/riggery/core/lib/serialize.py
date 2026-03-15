95c08f5f325ad44c755b798d036917548af62cf9
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2026-03-15 11:55:47 +0200

    reverted to simpler impl

--- src/riggery/core/lib/serialize.py
+++ src/riggery/core/lib/serialize.py
@@ -3,20 +3,20 @@ import maya.api.OpenMaya as om
 from ..elem import Elem
 from ...general import serialize as _ser
 
-def handler(x):
-    if isinstance(item, (om.MVector,
-                         om.MMatrix,
-                         om.MQuaternion,
-                         om.MEulerRotation)):
-        return list(item)
-
-    if isinstance(item, om.MPoint):
-        return list(item)[:-3]
-
-    if isinstance(item, Elem):
-        return str(item)
-
-    raise TypeError
-
 def simplify(item):
+    def handler(x):
+        if isinstance(item, (om.MVector,
+                             om.MMatrix,
+                             om.MQuaternion,
+                             om.MEulerRotation)):
+            return list(item)
+
+        if isinstance(item, om.MPoint):
+            return list(item)[:-3]
+
+        if isinstance(item, Elem):
+            return str(item)
+
+        raise TypeError
+
     return _ser.simplify(item, handler)
