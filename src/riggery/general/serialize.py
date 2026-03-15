a9871992dfd58d231c39112fc8cf70f0518d5016
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2026-03-15 08:24:47 +0200

    Added handler option

--- src/riggery/general/serialize.py
+++ src/riggery/general/serialize.py
@@ -1,3 +1,4 @@
+from typing import Callable, Optional
 import collections.abc as _abc
 
 def is_hashable(x) -> bool:
@@ -7,13 +8,22 @@ def is_hashable(x) -> bool:
     except TypeError:
         return False
 
-def simplify(value):
+def simplify(value, handler:Optional[Callable]=None):
     """
     Attempts to simplify *value* into some variant of a basic, JSON-supported
     type, such as int, float, list, dict etc.
 
-    :raises TypeError: can't simplify value
+    :param handler: if provided, should be a callable that will receive *value*
+        as its argument; this will be run first, and should throw
+        :class:`TypeError` for any cases it doesn't cover; defaults to None
+    :raises TypeError:
     """
+    if handler is not None:
+        try:
+            return handler(value)
+        except TypeError:
+            pass
+
     if isinstance(value, (int, float, str)):
         return value
 
@@ -21,9 +31,10 @@ def simplify(value):
         return None
 
     if isinstance(value, _abc.Mapping):
-        return {simplify(k): simplify(v) for k, v in value.items()}
+        return {simplify(k, handler): simplify(v, handler)
+                for k, v in value.items()}
 
     if isinstance(value, (_abc.Iterable, _abc.Iterator)):
-        return [simplify(member) for member in value]
+        return [simplify(member, handler) for member in value]
 
-    raise TypeError("can't simplify value")
+    raise TypeError("couldn't simplify {}".format(value))
