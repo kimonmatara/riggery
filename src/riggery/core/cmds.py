1399c6a62fa8cec4131f50e3c2677eb8504868c4
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2026-05-06 10:47:48 +0300

    Added file open utils

--- src/riggery/core/cmds.py
+++ src/riggery/core/cmds.py
@@ -1,5 +1,61 @@
 """This is auto-populated with wrapped commands from maya.cmds on startup."""
 
+from typing import Optional
+from pathlib import Path
 from .wrap import *
+from ..general.functions import short
+from .nodetypes import __pool__ as _nodes
+import maya.cmds as m
 
-# Add any custom commands here.
+def openScene(path) -> None:
+    path = Path(path)
+    longType = {'.ma': 'mayaAscii', '.mb': 'mayaBinary'}[path.suffix]
+    m.file(path.as_posix(),
+           typ=longType,
+           ignoreVersion=True,
+           o=True,
+           f=True,
+           prompt=False,
+           options='v=0;')
+
+@short(namespace='ns')
+def importScene(path, namespace:Optional[str]=None) -> None:
+    path = Path(path)
+    longType = {'.ma': 'mayaAscii', '.mb': 'mayaBinary'}[path.suffix]
+    kwargs = {}
+
+    if namespace:
+        kwargs['namespace'] = namespace
+    else:
+        kwargs['rpr'] = path.stem
+
+    m.file(path.as_posix(),
+           i=True,
+           typ=longType,
+           f=True,
+           prompt=False,
+           options='v=0;',
+           ignoreVersion=True,
+           mergeNamespacesOnClash=False,
+           pr=True,
+           **kwargs)
+
+@short(namespace='ns')
+def referenceScene(path, namespace:Optional[str]=None) -> '_nodes.Reference':
+    path = Path(path)
+    longType = {'.ma': 'mayaAscii', '.mb': 'mayaBinary'}[path.suffix]
+
+    if not namespace:
+        namespace = path.stem
+
+    result = m.file(path.as_posix(),
+                    r=True,
+                    typ=longType,
+                    prompt=False,
+                    options='v=0;',
+                    ignoreVersion=True,
+                    ns=namespace,
+                    gl=True,
+                    mergeNamespacesOnClash=False)
+
+    return _nodes['Reference'](m.referenceQuery(result, referenceNode=True))
